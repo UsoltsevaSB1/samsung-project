78d2729d18a469001ce350bfc798cb2aa882174c
usoltsevas <usoltsevaserafima@gmail.com>
2025-05-20 05:28:04 +0300

    добавила страницу со списком меток

--- app/src/main/java/com/example/mysportik/Marker.java
+++ app/src/main/java/com/example/mysportik/Marker.java
@@ -1,13 +1,22 @@
 package com.example.mysportik;
 
 public class Marker {
+    private String id;
     public double lat;
     public double lon;
     public String name;
+    private String note;
+    private String status; // "public" или "private"
+    private String userId;
+    private long timestamp;
 
-    public Marker(double lat, double lon, String name) {
-        this.lat = lat;
-        this.lon = lon;
-        this.name = name;
-    }
+//
+    public Marker() {}
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public double getLatitude() { return lat; }
+    public double getLongitude() { return lon; }
+    public String getNote() { return note; }
+    public String getStatus() { return status; }
 }
--- app/src/main/java/com/example/mysportik/MarksActivity.java
+++ app/src/main/java/com/example/mysportik/MarksActivity.java
@@ -1,25 +1,17 @@
 package com.example.mysportik;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mysportik.databinding.ActivityMarksBinding;
-import com.example.mysportik.databinding.ActivityRegisterBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -27,16 +19,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.example.mysportik.GeohashTable;
+import android.widget.ImageButton;
+import android.view.View;
+import android.content.Intent;
+
+
 
 public class MarksActivity extends AppCompatActivity {
 
     private GeohashStorage storage;
     private GeohashTable geohashTable;
-    EditText center_height2, center_latitude2, marker_text;
+    EditText center_name, center_height2, center_latitude2, marker_text;
     RadioButton public_marker;
 
 
@@ -62,6 +57,18 @@ public class MarksActivity extends AppCompatActivity {
 
         geohashTable = new GeohashTable(7);
 
+        // Находим кнопку по ID
+        Button find_marks2 = findViewById(R.id.find_marks2);
+        // Обработчик клика
+        find_marks2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для перехода на LoginActivity
+                Intent intent = new Intent(MarksActivity.this, UserMarksActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
     public void saveMarker(View view) {
@@ -75,12 +82,13 @@ public class MarksActivity extends AppCompatActivity {
         String marker_text = binding.markerText.getText().toString().trim();
         String center_latitude2 = binding.centerLatitude2.getText().toString().trim();
         String center_height2 = binding.centerHeight2.getText().toString().trim();
+        String center_name = binding.centerName.getText().toString().trim();
         boolean isPublic = binding.publicMarker.isChecked();
 
 
 
         // Валидация полей
-        if (marker_text.isEmpty() || center_latitude2.isEmpty() || center_height2.isEmpty()) {
+        if (marker_text.isEmpty() || center_latitude2.isEmpty() || center_name.isEmpty()) {
             Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -94,6 +102,7 @@ public class MarksActivity extends AppCompatActivity {
 
             // Создаем объект метки
             HashMap<String, Object> marker = new HashMap<>();
+            marker.put("name", center_name);
             marker.put("latitude", lat);
             marker.put("longitude", lon);
             marker.put("geohash", geohash);
@@ -153,10 +162,12 @@ public class MarksActivity extends AppCompatActivity {
             Toast.makeText(this, "Неизвестная ошибка: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
     private void clearFields() {
         binding.markerText.setText("");
         binding.centerLatitude2.setText("");
         binding.centerHeight2.setText("");
+        binding.centerName.setText("");
         binding.publicMarker.setChecked(false);
     }
 }
