1b4295b483b6d65981b9d20e445215bc0af68fbd
Your Name <you@example.com>
2025-07-03 00:58:09 +0300

    подключен firebase storage

--- app/src/main/java/com/example/mysportik/MapMarksActivity.java
+++ app/src/main/java/com/example/mysportik/MapMarksActivity.java
@@ -28,6 +28,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
